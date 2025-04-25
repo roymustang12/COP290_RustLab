353ce383df4c73828ccc6b9e1a02d101260b02de
Mannat <162910342+mannatgarg1608@users.noreply.github.com>
2025-04-25 23:26:13 +0530

    Add files via upload

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
-fn main() {
+#[cfg(feature = "main2")]
+fn main2() {
     lalrpop::Configuration::new()
         .generate_in_source_tree()
-        .process().unwrap();
+        .process()
+        .unwrap();
+}
+fn main() {
+    #[cfg(feature = "main2")]
+    main2();
 }
